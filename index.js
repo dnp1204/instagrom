16b99418fb486f25e1d79fcee9dbf7ac9a9a2fc1
dnp <doanhpham1995@gmail.com>
2017-08-03 17:05:35 +0700

    test heroku

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req,res) => {
-  res.send({ hi: 'there' });
+  res.send({ hi: 'buddy' });
 });
 
 const PORT = process.env.PORT || 5000;
