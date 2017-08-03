5493b6ae5402eac6e83d30a8f4c7416da7f0a4fc
dnp <doanhpham1995@gmail.com>
2017-08-04 00:43:04 +0700

    finished section 8

--- config/dev.js
+++ config/dev.js
@@ -2,5 +2,7 @@ module.exports = {
   googleClientID: '643326354318-pibdbmg1e0p60qpf0acs257s7dptpria.apps.googleusercontent.com',
   googleClientSecret: 'YM1EfdtS7yBgSfzVcn5ShrFG',
   mongoURI: 'mongodb://doanh:password@ds161049.mlab.com:61049/emaily-dev',
-  cookieKey: 'adsdfasdflkasjfrwieuhfcbdsjkvlasbfwefnafc'
+  cookieKey: 'adsdfasdflkasjfrwieuhfcbdsjkvlasbfwefnafc',
+  stripePublishableKey: 'pk_test_zKVUpzrInL7nSMMpHdAbf7tV',
+  stripeSecretKey: 'sk_test_q2iwPlciNjILeYj169m5WJcR'
 }
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -14,12 +14,13 @@ module.exports = app => {
     res.redirect('/surveys');
   });
 
-  app.get('/api/current_user', (req, res) => {
-    res.send(req.user);
-  });
-
   app.get('/api/logout', (req, res) => {
     req.logout();
     res.redirect('/');
   });
+  
+  app.get('/api/current_user', (req, res) => {
+    res.send(req.user);
+  });
+
 };
