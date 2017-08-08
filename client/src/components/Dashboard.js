5321bbdeb70151c4dc26b35155331b5718d579d6
dnp <doanhpham1995@gmail.com>
2017-08-08 23:04:03 +0700

    finish section 13

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import SurveyList from './surveys/SurveyList';
 
 class Dashboard extends Component {
   render() {
     return (
-        <div className="fixed-action-btn">
-          <Link to="/surveys/new" className="btn-floating btn-large red">
-            <i className="material-icons">add</i>
-          </Link>
+        <div>
+          <SurveyList />
+          <div className="fixed-action-btn">
+            <Link to="/surveys/new" className="btn-floating btn-large red">
+              <i className="material-icons">add</i>
+            </Link>
+          </div>
         </div>
     );
   }
--- client/src/index.js
+++ client/src/index.js
@@ -9,8 +9,8 @@ import App from './components/App';
 import reducers from './reducers';
 
 // Two line below is to test whether survey router can upload survey or not
-// import axios from 'axios';
-// window.axios = axios;
+import axios from 'axios';
+window.axios = axios;
 
 const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
 
