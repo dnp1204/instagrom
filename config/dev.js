253100f21ca1b7f5c0350a1eef2616e21e98d25b
dnp <doanhpham1995@gmail.com>
2017-10-26 10:51:11 -0400

    fix modal style

--- config/dev.js
+++ config/dev.js
@@ -1,6 +1,6 @@
 module.exports = {
   googleClientID: '333996789046-4f1udarijeunsld8ta1ohv56otbloia7.apps.googleusercontent.com',
   googleClientSecret: 'FHCr6QNQ1p5MfBb4bWijRDE0',
-  mongoURI: 'mongodb://localhost/instagram',
+  mongoURI: 'mongodb://localhost/instagrom',
   cookieKey: 'adsdfasdflkasjfrwieuhfcbdsjkvlasbfwefnafc',
-}
+};
