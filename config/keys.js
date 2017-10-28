07b871aae640be76e9294679ce09976310af92cf
dnp <doanhpham1995@gmail.com>
2017-10-28 19:08:14 -0400

    build for deploying

--- config/keys.js
+++ config/keys.js
@@ -1,8 +1,5 @@
-// keys.js - figure out what set of credentials to return
 if (process.env.NODE_ENV === 'production') {
-  // We are in production - return the prod set of keys
   module.exports = require('./prod');
 } else {
-  // we are in development - return the dev keys!!!
   module.exports = require('./dev');
 }
