9b0ea8aafe35e99aac51e7e91f137f936c3ce5cb
dnp <doanhpham1995@gmail.com>
2017-10-29 10:43:00 -0400

    fix login page issue

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,18 +1,45 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { BrowserRouter, Route } from 'react-router-dom';
 import { fetchCurrentUser } from '../actions';
-import Home from './Home';
+import LoginPage from './LoginPage';
+import Header from './header/Header';
+import Profile from './profile/Profile';
+import Newsfeed from './newsfeed/Newsfeed';
 
 class App extends Component {
   componentWillMount() {
     this.props.fetchCurrentUser();
   }
 
+  authRoute() {
+    return (
+      <div>
+        <Header />
+        <div className="app-container">
+          <Route path="/" exact component={Newsfeed} />
+          <Route path="/profile" exact component={Profile} />
+          <Route path="/profile/:id" exact component={Profile} />
+        </div>
+      </div>
+    );
+  }
+
+  noAuthRoute() {
+    return <Route path="/" exact component={LoginPage} />;
+  }
+
   render() {
     return (
-      <Home />
+      <BrowserRouter>
+        {this.props.user._id ? this.authRoute() : this.noAuthRoute()}
+      </BrowserRouter>
     );
   }
 }
 
-export default connect(null, { fetchCurrentUser })(App);
+function mapStateToProps(state) {
+  return { user: state.user };
+}
+
+export default connect(mapStateToProps, { fetchCurrentUser })(App);
