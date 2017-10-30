fc708ea64abd55c61a55a9809715030bee09604b
dnp <doanhpham1995@gmail.com>
2017-10-30 19:36:56 -0400

    display no posts

--- client/src/components/newsfeed/Newsfeed.js
+++ client/src/components/newsfeed/Newsfeed.js
@@ -15,46 +15,51 @@ class Newsfeed extends Component {
 
   renderPosts() {
     const { following, user } = this.props;
-    return following.map(post => {
-      let liked = false;
-      for (let i = 0; i < post.likes.length; i++) {
-        if (post.likes[i]._id === user._id) {
-          liked = true;
-          break;
+    if (following.length === 0) {
+      return <div style={{ textAlign: 'center' }}>There are no posts to show yet</div>;
+    } else {
+      return following.map(post => {
+        let liked = false;
+        for (let i = 0; i < post.likes.length; i++) {
+          if (post.likes[i]._id === user._id) {
+            liked = true;
+            break;
+          }
         }
-      }
 
-      return (
-        <NewsfeedPost
-          currentUserId={user._id}
-          key={post._id}
-          liked={liked}
-          userAvatar={post.userAvatar}
-          userName={post.userName}
-          imageLink={post.image}
-          likes={post.likes}
-          comments={post.comments}
-          createdAt={post.createdAt}
-          gotoUser={() => this.props.history.push(`/profile/${post.userId}`)}
-          likeFollowingPost={() =>
-            this.props.likeFollowingPost(
-              post._id,
-              post.userId,
-              post.userName,
-              post.userAvatar
-            )}
-          commentFollowingPost={content =>
-            this.props.commentFollowingPost(
-              content,
-              post._id,
-              post.userId,
-              post.userName,
-              post.userAvatar
-            )}
-          deleteComment={(commentId) => this.props.deleteComment(post._id, commentId)}
-        />
-      );
-    });
+        return (
+          <NewsfeedPost
+            currentUserId={user._id}
+            key={post._id}
+            liked={liked}
+            userAvatar={post.userAvatar}
+            userName={post.userName}
+            imageLink={post.image}
+            likes={post.likes}
+            comments={post.comments}
+            createdAt={post.createdAt}
+            gotoUser={() => this.props.history.push(`/profile/${post.userId}`)}
+            likeFollowingPost={() =>
+              this.props.likeFollowingPost(
+                post._id,
+                post.userId,
+                post.userName,
+                post.userAvatar
+              )}
+            commentFollowingPost={content =>
+              this.props.commentFollowingPost(
+                content,
+                post._id,
+                post.userId,
+                post.userName,
+                post.userAvatar
+              )}
+            deleteComment={commentId =>
+              this.props.deleteComment(post._id, commentId)}
+          />
+        );
+      });
+    }
   }
 
   render() {
